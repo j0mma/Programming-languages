ca9905683c2f9e7f79b3888a464bf2a3825db625
j0mma <masiala.jo@gmail.com>
2021-01-25 22:13:59 -0300

    Added Exercise2-2.c which contains answer for exercise 2 in chapter 2.

---
+++ C/book-answers/programming-in-c/chap2/Exercise2-2.c
@@ -0,0 +1,17 @@
+/*!****************************************************************************
+ * @file Exercise2-2.c
+ * @brief Answer to exercise 2 from chapter 2.
+******************************************************************************/
+
+#include <stdio.h>
+
+int main(void)
+{
+
+	printf("In C, lowercase letters are significants.\n");
+	printf("main() is where program execution begins.\n");
+	printf("Opening and closing braces encloses program statements in a routine.\n");
+	printf("All program statements must be terminated by a semicolon.\n");
+
+	return 0;
+}
