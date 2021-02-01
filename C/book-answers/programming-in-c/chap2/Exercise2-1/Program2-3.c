2f1c7279901fc99cc2907b1bd6a624e0db4a44f1
Joël <72325648+j0mma@users.noreply.github.com>
2021-02-01 20:04:01 -0300

    Update Program2-3.c

--- C/book-answers/programming-in-c/chap2/Exercise2-1/Program2-3.c
+++ C/book-answers/programming-in-c/chap2/Exercise2-1/Program2-3.c
@@ -1,6 +1,12 @@
+/*!****************************************************************************
+ * @file Program2-3.c
+ * @brief Displaying multiple lines of output
+******************************************************************************/
+
 #include <stdio.h>
 
-int main(void){
+int main(void)
+{
 
 	printf("Testing...\n..1\n...2\n....3\n");
 
