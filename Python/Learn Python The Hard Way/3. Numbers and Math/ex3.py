3b27b715db8e9dc98cbb1d4bd2321c773494bbf9
j0mma <masiala.jo@gmail.com>
2021-03-15 21:11:16 -0300

    Added Exercises answers in Learn Python 3 The Hard Way

---
+++ Python/Learn Python The Hard Way/3. Numbers and Math/ex3.py
@@ -0,0 +1,24 @@
+print("I will now count my chickens:") # Printing some text
+
+print("Hens", 25 + 30 / 6) # Printing the result of the operation after the text
+print("Roosters", 100 - 25 * 3 % 4) # Same as line 2
+
+print("Now I will count the eggs:") # Same as line 1
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) # Printing the evaluation of the expression between ()
+
+print("Is it true that 2 + 2 < 5 - 7?") # Same as line 1
+
+print(3 + 2 < 5 - 7) # Printing the evaluation of the expression between ()
+
+print("What is 3 + 2?", 3 + 2) # Printing the result of the operation after the text 
+print("What is 5 - 7", 5 - 7) # Printing the result of the operation after the text
+
+print("Oh that's why it's False.") # Printing some text
+
+print("How about some more.") # Printing some text
+
+print("Is it greater?", 5 > -2) # Printing the result of the operation after the text
+print("Is it greater or equal?", 5 >= -2) # Printing the result of the operation after the text
+print("Is it less than or equal", 5 <= -2) # Printing the result of the operation after the text
+
