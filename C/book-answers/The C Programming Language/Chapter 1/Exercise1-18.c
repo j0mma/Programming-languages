989f425a8d949d70100feb6f8f58f3f87d5a9fa6
j0mma <masiala.jo@gmail.com>
2021-04-07 18:31:25 -0400

    Working on Exercise 1-18

--- C/book-answers/The C Programming Language/Chapter 1/Exercise1-18.c
+++ C/book-answers/The C Programming Language/Chapter 1/Exercise1-18.c
@@ -2,48 +2,47 @@
 #define MAXLINE 1000    /* maximum input line size */
 
 int getLine(char line[], int maxline);
-void copy(char to[], char from[]);
+void removeBlanks(char to[], char from[]);
 
 /* print longest input line */
 main()
 {
-    int len;            /* current line length */
-    int max;            /* maximum length seen so far */
+    int len;                /* current line length */
     char line[MAXLINE];     /* current input line */
-    char longest[MAXLINE];  /* longest line saved here */
+    char newline[MAXLINE];  /* longest line saved here */
 
-    max = 0;
-    while ((len = getLine(line, MAXLINE)) > 0) 
-        if (len > max) {
-            max = len;
-            copy(longest, line);
-        }
-    if (max > 0)        /* there was a line */
-       printf("%s", longest);
+    while ((len = getLine(line, MAXLINE)) > 0) { /* while the input is not empty */
+       removeBlanks(newline, line);
+       printf("%d, %s", len, newline);
+    }
     return 0;
 }
 
-/* getline: read a line into s, return length */
+/* getline: read a line into s, return length of line */
 int getLine(char s[], int lim)
 {
     int c, i;
 
-    for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i) /* insert character only if those 3 conditions are met before */
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) /* insert character only if those 3 conditions are met before */
         s[i] = c;
-        
-    if (c == '\n' || c == '\t') {
-            s[i] = '\b';
+    if (c == '\n') { /* when newline encountered */
+            s[i] = c;
             ++i;
     }
-    
     s[i] = '\0'; /* add null character to the array */
     return i;
 }
 
 /* copy: copy 'from' into 'to'; assume to is big enough */
-void copy(char to[], char from[])
+void removeBlanks(char to[], char from[])
 {
-    int i = 0;
-    while ((to[i] = from[i]) != '\0') /* copy content of from[] in to[] until null character is met */ 
+    int i = 0, blanks = 0, idx = 0;
+    while ((to[i] = from[i]) != '\0') {
+        if (from[i] == ' ' || from[i] == '\t'){
+            to[i] = '*';
+            ++blanks;
+        }
         ++i;
+    }
+    //for (i = blanks; to[i] !=;)
 }
