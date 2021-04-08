dc66a126c5df821990e8e864655b875fab7ca498
j0mma <masiala.jo@gmail.com>
2021-04-08 12:20:09 -0400

    Finished Exercise 1-18

--- C/book-answers/The C Programming Language/Chapter 1/Exercise1-18.c
+++ C/book-answers/The C Programming Language/Chapter 1/Exercise1-18.c
@@ -2,18 +2,17 @@
 #define MAXLINE 1000    /* maximum input line size */
 
 int getLine(char line[], int maxline);
-void removeBlanks(char to[], char from[]);
+void removeTrailingBlanks(char line[], int len);
 
 /* print longest input line */
 main()
 {
     int len;                /* current line length */
     char line[MAXLINE];     /* current input line */
-    char newline[MAXLINE];  /* longest line saved here */
 
-    while ((len = getLine(line, MAXLINE)) > 0) { /* while the input is not empty */
-       removeBlanks(newline, line);
-       printf("%d, %s", len, newline);
+    while ((len = getLine(line, MAXLINE)) > 0) { 
+       removeTrailingBlanks(line, len);
+       printf("%s", line);
     }
     return 0;
 }
@@ -23,26 +22,20 @@ int getLine(char s[], int lim)
 {
     int c, i;
 
-    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) /* insert character only if those 3 conditions are met before */
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) 
         s[i] = c;
-    if (c == '\n') { /* when newline encountered */
-            s[i] = c;
-            ++i;
+    if (c == '\n') { 
+        s[i] = c;
+        ++i;
     }
-    s[i] = '\0'; /* add null character to the array */
+    s[i] = '\0'; 
     return i;
 }
 
-/* copy: copy 'from' into 'to'; assume to is big enough */
-void removeBlanks(char to[], char from[])
+/* removeTrailingBlanks: remove trailing blanks space to the right */
+void removeTrailingBlanks(char line[], int len)
 {
-    int i = 0, blanks = 0, idx = 0;
-    while ((to[i] = from[i]) != '\0') {
-        if (from[i] == ' ' || from[i] == '\t'){
-            to[i] = '*';
-            ++blanks;
-        }
-        ++i;
-    }
-    //for (i = blanks; to[i] !=;)
+    int i;
+    for(i = len - 2; line[i] == ' ' || line[i] == '\t'; --i) 
+        line[i] = '\b';
 }
