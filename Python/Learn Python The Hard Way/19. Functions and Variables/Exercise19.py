98eb7a781c419940241f61912a9e40d033793da3
j0mma <masiala.jo@gmail.com>
2021-04-07 21:06:08 -0400

    Added answers

---
+++ Python/Learn Python The Hard Way/19. Functions and Variables/Exercise19.py
@@ -0,0 +1,31 @@
+# defining the function cheese and crackers
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+    print(f"You have {cheese_count} cheeses!")
+    print(f"You have {boxes_of_crackers} boxes of crackers!")
+    print("Man that's enough for a party!")
+    print("Get a blanket.\n")
+
+
+print("We can just give the function members directly:")
+# passing litteral numbers directly to the function
+cheese_and_crackers(20, 30)
+
+
+print("OR, we can use variables from our script:")
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+# passing variables to the functions
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+
+print("We can even do math inside too:")
+
+# doing some math operation with the numbers litterals passed as arguments
+cheese_and_crackers(10 + 20, 5 + 6)
+
+
+print("And we can combine the two, variables and math:")
+
+# doing math with variables and numbers litterals as arguments
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
