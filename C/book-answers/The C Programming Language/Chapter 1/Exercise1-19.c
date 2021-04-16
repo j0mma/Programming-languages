bf9dfe1a4dc1a0401927895ae04341ac65d1b3b6
j0mma <masiala.jo@gmail.com>
2021-04-16 14:29:30 -0400

    Modified Exercise 1-19 in The C Programming Language

--- C/book-answers/The C Programming Language/Chapter 1/Exercise1-19.c
+++ C/book-answers/The C Programming Language/Chapter 1/Exercise1-19.c
@@ -2,18 +2,19 @@
 #define MAXLINE 1000    /* maximum input line size */
 
 int getLine(char line[], int maxline);
-void copy(char from[], char to[])
-void reverse(char s[]);
+//void copy(char from[], char to[]);
+void reverse(char original[], char reversed[]);
 
 /* print longest input line */
 main()
 {
     int len;                /* current line length */
     char line[MAXLINE];     /* current input line */
+    char reversed[MAXLINE];
 
     while ((len = getLine(line, MAXLINE)) > 0) { 
-       reverse(line);
-       printf("%s", line);
+       reverse(line, reversed);
+       printf("%s", reversed);
     }
     return 0;
 }
@@ -42,10 +43,18 @@ void copy(char from[], char to[])
 }
 
 /* removeTrailingBlanks: remove trailing blanks space to the right */
-void reverse(char s[])
+void reverse(char original[], char reversed[])
 {
-    int i = 0;
-    
-        
+    int i = 0, len = 0;
+    while(original[i] != '\0') {
+        ++len;
+        ++i;
+    }
+    len -= 1;
+    i = 0;
+    while(len >= 0) {
+        reversed[i] = original[len];
+        ++i;
+        --len;
+    }
 }
-
